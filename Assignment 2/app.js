ff013ad5fd5984562bd544eda698f2cb64c8992d
zubaida-1112 <zubaidamali1990@gmail.com>
2025-09-04 04:12:34 -0700

    Initial commit with class folders

---
+++ Assignment 2/app.js
@@ -0,0 +1,70 @@
+// assignmentNumber2...
+
+
+// Question # 1
+
+// var username;
+
+// alert(username)
+
+// Question # 2
+
+// var myname = "Zubaida Muhammad Ali";
+
+// alert(myname);
+
+// Question # 3
+
+// var message;
+
+// message = "Hello World"
+
+// alert(message);
+
+
+// Question # 4
+
+// var studentbiodata = "Jhone Doe";
+
+// alert(studentbiodata);
+
+// var studentbiodata = "15 years old";
+
+// alert(studentbiodata);
+
+// var studentbiodata = "Certifiedd Mobile Application Development";
+
+// alert(studentbiodata);
+
+
+// Question # 5
+
+// var a = "PIZZA \nPIZZ \nPIZ \nPI \nP";
+
+// alert(a);
+
+
+// Question # 6
+
+// var email = "abc@gmail.com";
+
+// alert("My email address is" +"  " + email);
+
+
+// Question # 7
+
+// var book = "A smarter way to learn JavaScript";
+
+// alert(book);
+
+
+// Question # 8
+
+// document.write("Yah!I can write HTML content through JavaScript");
+
+
+// Question # 9
+
+// var design = " “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬” "
+
+// alert(design)
