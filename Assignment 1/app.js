f3bc0f733b14ed55874504a74c665fbbdab10bb6
zubaida-1112 <zubaidamali1990@gmail.com>
2025-09-04 08:27:26 -0700

    added chapter 1 as normal folder

---
+++ Assignment 1/app.js
@@ -0,0 +1,27 @@
+// Javascript Assignment1
+
+// external method...
+
+// question 1
+
+// alert("Welcome to JavaScript!")
+
+// Question 2
+
+// alert("Error! Please enter a valid password.");
+
+// Question 3
+
+// alert("Welcome to JS Land...\nHappy Coding!")
+
+// Question 4
+
+// alert("Welcome to JS Land...")
+
+// // Question 5
+
+// alert("Happy Coding! \nPrevent this page from creating additional dialogs.")
+
+// Question 6
+
+console.log("Hello... I can run JS through my web browser's console")
