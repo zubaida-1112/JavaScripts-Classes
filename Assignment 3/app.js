8ed67525257d56707d15f0a2558bf86541628480
zubaida-1112 <zubaidamali1990@gmail.com>
2025-09-04 08:16:57 -0700

    Submiting Assignment 4 & 5

---
+++ Assignment 3/app.js
@@ -0,0 +1,37 @@
+// Assignment 3:
+
+// Question 1:
+
+// let age = 21;
+
+// alert(age);
+
+// Question 2:
+
+// let visits = 14;
+
+// alert("You have visited this site" + " "+ visits+ " " + "times.")
+
+// Question 3:
+
+// let birthYear = 2005;
+
+// document.write("My birth year is" + " " + birthYear + "<br />" + "Data type of my declared variable is number");
+
+// Question 4:
+
+// let visitiorName;
+
+// let productTitle;
+
+// let Quantity;
+
+// visitiorName = "John Doe"
+// productTitle = "Ordered 5 T-Shirt(s)"
+// Quantity = "XYZ Clothing Stores" 
+
+// document.write(visitiorName + " "+ productTitle + " " + Quantity);
+
+
+
+// Completed...
---
+++ Assignment 4/app.js
@@ -0,0 +1,33 @@
+// Question 1:
+
+// let username = "JohnDoe", age = 20, city = "New York";
+
+// Question 2:
+
+// Legal Variable Names:
+
+// 1. myVariable
+// 2. name1
+// 3. _hello
+// 4. $price
+// 5. user_name (snake case)
+
+// Illegal Variable Names:
+
+// 1. 1variable (starts with a number)
+// 2. my-variable (contain a hyphen)
+// 3. let (reserved keywords)
+// 4. @email (contain a special characters other than _ or $)
+// 5. my variable (contain space between words)
+
+
+// Question 3:
+
+// let heading = "<h1> Rules for naming JS variables </h1>";
+// let FB1 = "Variable name can only contain , numbers , $ , and _. For example: $my_1stVariable"
+// let FB2 = "Variables must begin with a letter, $ or_. For example: $name,_name or name"
+// let FB3 = "Variables name are case sensative"
+// let FB4 = "Variable names should not be JS keywords"
+
+
+// document.write(heading +"<br />" + FB1 +"<br />" +FB2 +"<br />" +FB3 +"<br />" +FB4);
