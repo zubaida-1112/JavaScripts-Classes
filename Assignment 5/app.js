5c963ab8710494540af96e58c51313b5224c4e73
zubaida-1112 <zubaidamali1990@gmail.com>
2025-09-13 03:19:54 -0700

    Assignment 5 completed

---
+++ Assignment 5/app.js
@@ -0,0 +1,305 @@
+// Assignment # 05
+
+// Following are five tasks in question # 1
+
+// Question # 1
+//Addition 
+
+
+// let number1 = 3
+// let number2 = 5
+// sum = (number1+number2)
+
+// document.write(`Sum of 3 and 5 is ${sum}`)
+
+// Task # 2
+// Subtraction
+
+
+// let number1 = 3
+// let number2 = 5
+// sub = (number1-number2)
+
+// document.write(`Subtract of 3 and 5 is ${sub}`)
+
+
+// Task # 3
+// Multiplication
+
+
+// let number1 = 3
+// let number2 = 5
+// multiply = (number1*number2)
+
+// document.write(`Multiplication of 3 and 5 is ${multiply}`)
+
+
+// Task # 4
+// Division
+
+
+// let number1 = 3
+// let number2 = 5
+// divide = (number1/number2)
+
+// document.write(`Division of 3 and 5 is ${divide}`)
+
+
+// Task # 5
+// Modulus
+
+
+// let number1 = 3
+// let number2 = 5
+// Modulus = (number1%number2)
+
+// document.write(`Modulus of 3 and 5 is ${Modulus}`)
+
+// ....Task End....
+
+
+// Question # 3
+
+// let value;
+
+// document.write(`Value after variable declaration is: ${value}`)
+
+// document.write(`<br />`)
+
+// value = 5;
+// document.write(`Initial value: ${value}`)
+// document.write(`<br />`)
+
+// value++;
+// document.write(`Value after increment is: ${value}`)
+// document.write(`<br />`)
+
+// value = value + 7;
+// document.write(`Value after addition is: ${value}`)
+// document.write(`<br />`)
+
+// value--;
+// document.write(`Value after decrement is: ${value}`)
+// document.write(`<br />`)
+
+// let remainder = value % 3;
+// document.write(`The remainder is: ${remainder}`)
+
+
+
+// Question # 4
+
+// let price = 600;
+// price = price*5;
+
+// document.write(`Total cost to buy 5 tickets to a movie is ${price}PKR`)
+
+
+// Question # 5
+// Table of 4:
+
+// let num = 1;
+
+// document.write(`4 X ${num} = 4`)
+// document.write(`<br />`)
+// num++;
+
+// document.write(`4 X ${num} = 8`)
+// document.write(`<br />`)
+// num++;
+
+// document.write(`4 X ${num} = 12`)
+// document.write(`<br />`)
+// num++;
+
+// document.write(`4 X ${num} = 16`)
+// document.write(`<br />`)
+// num++;
+
+// document.write(`4 X ${num} = 20`)
+// document.write(`<br />`)
+// num++;
+
+// document.write(`4 X ${num} = 24`)
+// document.write(`<br />`)
+// num++;
+
+// document.write(`4 X ${num} = 28`)
+// document.write(`<br />`)
+// num++;
+
+// document.write(`4 X ${num} = 32`)
+// document.write(`<br />`)
+// num++;
+
+// document.write(`4 X ${num} = 36`)
+// document.write(`<br />`)
+// num++;
+
+// document.write(`4 X ${num} = 40`)
+// document.write(`<br />`)
+// num++;
+
+
+
+// Question # 6
+
+// celsius to fahrenheit
+
+// let celsius = 25;
+// let fahrenheit = 70;
+
+// let total2 = (celsius*9/5) + 32;
+// document.write(`25\u00B0C is ${total2}\u2109`)
+
+// document.write(`<br />`);
+
+
+// let total = (fahrenheit-32) * 5/9;
+// document.write(`70\u2109 is ${total}\u00B0C`);
+
+
+// Question # 7
+
+// document.write(`<h1> Shopping Cart </h1>`)
+
+
+// let price1 = 650;
+// let price2 = 100;
+
+// let quan1 = 3;
+// let quan2 = 7;
+
+// let shippingCharges = 100;
+
+// document.write(`Price of item 1 is ${price1}`);
+// document.write(`<br />`);
+// document.write(`Quantity of item 1 is ${quan1}`);
+// document.write(`<br />`);
+// document.write(`Price of item 2 is ${price2}`);
+// document.write(`<br />`);
+// document.write(`Quantity of item 2 is ${quan2}`);
+// document.write(`<br />`);
+// document.write(`Shipping Charges ${shippingCharges}`)
+// document.write(`<br />`);
+// document.write(`<br />`);
+
+// let total = (price1*quan1) + (price2*quan2) + (shippingCharges);
+
+// document.write(`Total cost of your order is ${total}`);
+
+
+// Question # 8
+
+// document.write(`<h1> Marks Sheet </h1>`)
+// document.write(`<br />`);
+// document.write(`<br />`);
+// document.write(`<br />`);
+
+// let totalMarks = 980;
+// let marksObtained = 804;
+
+// document.write(`Total marks: ${totalMarks}`);
+// document.write(`<br />`);
+// document.write(`Marks obtained: ${marksObtained}`);
+// document.write(`<br />`);
+
+// let total = (804/980*100); 
+// document.write(`Percentage: ${total}%`);
+
+
+// Question # 9
+
+// document.write(`<h1>Currency in PKR</h1>`)
+// document.write(`<br />`);
+// document.write(`<br />`);
+
+// let usd = 284;
+// let riyal = 75.06;
+
+// let quan1 = 10;
+// let quan2 = 25;
+// let total = (usd*quan1) + (riyal*quan2);
+
+// document.write(`Total Currency in PKR: ${total}`)
+
+
+// Question # 10
+
+// let num = 5;
+
+// let total = (5+5*10/2);
+// console.log(total);
+
+
+// Question # 11
+
+// document.write(`<h1>Age Calculator</h1>`)
+// document.write(`<br />`);
+// document.write(`<br />`);
+
+// let currentYear = 2025;
+// let birthYear = 2005;
+// let total = (2025-2005);
+
+// document.write(`Current Year : ${currentYear}`);
+// document.write(`<br />`);
+
+// document.write(`Birth Year : ${birthYear}`);
+// document.write(`<br />`);
+
+// document.write(`Your Age is : ${total}`);
+
+
+
+// Question # 12
+
+// let radius = 20;
+// let pi = 3.142;
+// let circumference = 2 * pi * radius;
+// let area = pi * radius * radius;
+
+
+// document.write(`<h1>The Geometrizer</h1>`);
+// document.write(`<br />`);
+// document.write(`<br />`);
+
+// document.write(`Radius of a circle: ${radius}`);
+// document.write(`<br />`);
+
+// document.write(`The circumference is :${circumference}`);
+// document.write(`<br />`);
+
+// document.write(`The area is: ${area}`);
+
+
+// Question # 13
+
+
+// let snacks = "chips";
+// let age = 20;
+// let maximumAge = 50;
+// let estimatedAmount = 2;
+
+// let yearsLeft = (maximumAge - age);
+// let estimatedAmountConsumed = (yearsLeft * estimatedAmount);
+
+
+// document.write(`<h1>The Lifetime Supply Calculator</h1>`);
+// document.write(`<br />`);
+// document.write(`<br />`);
+
+// document.write(` Favourite Snack: ${snacks}`);
+// document.write(`<br />`);
+
+// document.write(`Current age: ${age}`);
+// document.write(`<br />`);
+
+// document.write(`Estimated Maximum Age: ${maximumAge}`);
+// document.write(`<br />`);
+
+// document.write(`Amount of snacks per day: ${estimatedAmount}`);
+// document.write(`<br />`);
+
+// document.write(`You will need ${estimatedAmountConsumed} ${snacks} to last you until the ripe old age of ${maximumAge}`);
+
