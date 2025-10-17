fb041594dca6396197720ed86af824f754920f13
zubaida-1112 <zubaidamali1990@gmail.com>
2025-10-17 00:00:20 -0700

    Chapter 26 to 30

---
+++ Assignment 12/app.js
@@ -0,0 +1,89 @@
+
+
+// Question #1
+
+// Solution:
+
+// let num = Number(prompt("Enter a positive integer:"));
+
+// if (num < 0) {
+//     alert("Please enter a positive integer.");
+// }
+// else {
+//     document.write(`number: ${num}<br />`);
+//     document.write(`round off value: ${Math.round(num)}<br />`);
+//     document.write(`floor value: ${Math.floor(num)}<br />`);
+//     document.write(`ceil value: ${Math.ceil(num)}<br />`)
+// }
+
+// Question #2
+
+// Solution:
+
+// let num = Number(prompt(`Enter a negative integer:`));
+
+// if(num >= 0){
+//     alert("Please enter a negative integer.");
+// }
+// else{
+//     document.write(`number: ${num}<br />`);
+//     document.write(`round off: ${Math.round(num)}<br />`);
+// document.write(`floor value: ${Math.floor(num)}<br />`);
+// document.write(`ceil value: ${Math.ceil(num)}<br />`);
+// }
+
+// Question #3
+
+// Solution:
+
+// let num = Number(prompt("Enter a number:"));
+// document.write(`The absolute value of ${num} is ${Math.abs(num)}`);
+
+// Question #4
+
+// Solution:
+
+// let randomDice = Math.floor(Math.random() * 6) + 1;
+// document.write(`Random dice value: ${randomDice}`);
+
+// Question #5
+
+// Solution:
+
+// let randomCoin = Math.floor(Math.random() *2) +1;
+
+// if(randomCoin === 2){
+//     document.write(`${randomCoin} <br /> Random coin value: Heads`);
+// }
+// else{
+//     document.write(`${randomCoin} <br /> Random coin value: Tails`);
+// }
+
+// Question #6
+
+// Solution:
+
+// let randomNum = Math.floor(Math.random()*100)+1;
+// document.write(`Random number between 1 and 100: ${randomNum}`);
+
+
+// Question #7
+
+// Solution:
+
+// let userWeight = Number(prompt(`Enter your weight in kilograms:`));
+// document.write(`The weigth of user is ${userWeight} kilograms`);
+
+// Question #8
+
+// Solution:
+ 
+// let secretNum = Math.floor(Math.random()*10)+1;
+// let userGuess = Number(prompt(`Enter a number between 1 to 10:`));
+// if(userGuess === secretNum){
+//     alert("Congratulations! You guessed the secret number.");
+// }
+// else{
+//     alert(`Try again! The secret number was ${secretNum}.`);
+// }
+
